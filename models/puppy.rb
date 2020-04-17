73ac7a8ca64e90f2f3cfa9d255f5d9b64c1dea76
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-17 23:47:48 +0000

    Automatically backed up by Learn

--- models/puppy.rb
+++ models/puppy.rb
@@ -4,3 +4,9 @@ class Puppy
 
 
 end
+#   can create a new instance of the puppy class (FAILED - 1)
+#   can read a puppy name (FAILED - 2)
+#   can read a puppy breed (FAILED - 3)
+#   can read a puppy age in months (puppy#age) (FAILED - 4)
+#   can change puppy age in months (puppy#age=) (FAILED - 5)
+#   can change puppy name (FAILED - 6)
