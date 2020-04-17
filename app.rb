303edd548981744a42f5af45f2536d10bc91e472
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-17 23:54:16 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -11,4 +11,21 @@ class App < Sinatra::Base
     erb :create_puppy
   end
 
+  post '/puppy' do
+    erb :display_puppy
+  end
+
 end
+
+# App
+#   GET /
+#     sends a 200 status code
+#     has a link with the text "List A Puppy"
+#     has a link to list a puppy that links to /new
+#   GET /new
+#     sends a 200 status code
+#     renders a form that can POST a name, breed, and age
+#   POST /puppy
+#     sends a 200 status code
+#     recieves a request from /new at /puppy
+#     displays the puppy information dynamically
